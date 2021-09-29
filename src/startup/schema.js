ee580c151be647a344df4dd3c050549360863e93
NistoLeo <nistorica.leonard@gmail.com>
2021-09-29 17:12:16 +0300

    29 09 graphql dictionaries eod

--- src/startup/schema.js
+++ src/startup/schema.js
@@ -1,19 +1,23 @@
 const { makeExecutableSchema } = require('apollo-server-koa')
 const merge = require('lodash.merge')
-
+const helloWorldResolvers = require('../features/helloWorld/resolvers');
 const { loadTypedefsSync } = require('@graphql-tools/load')
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
 const { join } = require('path')
 
 const userResolvers = require('../features/user/resolvers')
+const conferenceResolvers = require('../features/conference/resolvers');
+const dictionaryResolvers = require('../features/dictionaries/resolvers');
 
 const oldTypeDefs = []
 const sources = loadTypedefsSync(join(__dirname, '../**/*.graphql'), {
   loaders: [new GraphQLFileLoader()]
 })
-const resolvers = merge(userResolvers)
+const resolvers = merge(userResolvers, helloWorldResolvers, conferenceResolvers, dictionaryResolvers)
 
 const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs]
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
 module.exports.tests = { typeDefs, resolvers }
+
+
