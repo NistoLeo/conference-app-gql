5b31d51f15fbe15c4f1281ed8207522d59a1e45e
NistoLeo <nistorica.leonard@gmail.com>
2021-10-01 13:39:03 +0300

    gql midday 1oct

--- src/features/dictionaries/resolvers.js
+++ src/features/dictionaries/resolvers.js
@@ -1,5 +1,10 @@
-const dictionaryResolvers ={
-
+const dictionaryResolvers = {
+    Query: {
+        typeList: async () => { return [] },
+        categoryList: async () => {return [] },
+        countryList: async () => { return []},
+        countyList: async () => {return [] },
+        cityList: async () => {return [] }
+    }
 }
-
 module.exports = dictionaryResolvers
